7011bc40b7d192edd2bc4edb650b7dd19338665f
Collin Chapman <cchap14@gmail.com>
2021-02-03 20:36:42 -0800

    separate business and user interface logic
    Co-authored-by: Theron Packus <tlpackus@gmail.com>
    >Co-authored-by: Carlos Urquiza <webquiza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,49 @@
+
+//Business Logic
+
+function fortune(array) {
+  if (array.length === 7) {
+    return 1
+  } else if (array.length < 7) {
+    return 2
+  } else if (array.length > 7) {
+    return 3
+  }
+}
+
+
+//User interface logic
+
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     
+    let array = [];
+
     $("input:checkbox[name=unlucky-events]:checked").each(function() {
       const unluckyEvent = $(this).val();
-
-
+      array.push(unluckyEvent);
     });
 
-    $("input:checkbok[name=lucky-events]:checked").each(function() {
+    $("input:checkbox[name=lucky-events]:checked").each(function() {
       const luckyEvent = $(this).val();
+      array.push(luckyEvent);
+    });
 
+    $("input:checkbox[name=miracle-events]:checked").each(function() {
+      const miracleEvent = $(this).val();
+      array.push(miracleEvent);
+    });
 
-    })
-  }
-})
+    let result = fortune(array)
+
+    if (result === 1) {
+      $("#fortune").text("Looks like your entire year is set! Way to go!")
+    } else if (result === 2) {
+      $("#fortune").text("Uhh ohh, things are not looking good...")
+    } else if (result === 3) {
+      $("#fortune").text("Just give up!")
+    }
+
+  });
+});
